b12874bbbc3d9fb8cd6efd8e64d587f7a60cb5d9
Ankita Arya <090ankita@gmail.com>
2022-10-04 12:11:42 +0530

    sawgger config
    added swagger configuration

--- src/main/kotlin/com/transform/urlshortner/UrlShortnerApplication.kt
+++ src/main/kotlin/com/transform/urlshortner/UrlShortnerApplication.kt
@@ -1,12 +1,25 @@
 package com.transform.urlshortner
 
+import com.google.common.base.Predicates
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@EnableSwagger2
 @SpringBootApplication
 open class UrlShortnerApplication
 
 	fun main(args: Array<String>) {
 		runApplication<UrlShortnerApplication>(*args)
+	}
+
+	@Bean
+	fun swaggerSpringMvcPlugin(): Docket? {
+		return Docket(DocumentationType.SWAGGER_2).select()
+			.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot"))).build()
 	}
 
